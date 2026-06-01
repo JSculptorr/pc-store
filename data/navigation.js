3461fbaf0b86d0286c330b0351ac9a10a388785d
JSculptor <alforss23@gmail.com>
2026-06-02 03:36:19 +0500

    Delete data directory

--- data/navigation.js
+++
@@ -1,6 +0,0 @@
-export const navItems = [
-  { label: 'Главная', href: '#hero' },
-  { label: 'Каталог', href: '#catalog' },
-  { label: 'Сервис', href: '#service-details' },
-  { label: 'Контакты', href: '#contact-preview' },
-]
